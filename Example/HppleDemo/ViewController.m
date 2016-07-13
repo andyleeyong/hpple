0c6a95d14c245bd2ee70bc935cab8d8667d3e44d
andyleeyong <andy451984648@gmail.com>
2016-07-13 22:05:35 +0800

    Demo 下厨房

--- Example/HppleDemo/ViewController.m
+++ Example/HppleDemo/ViewController.m
@@ -6,22 +6,86 @@
 //
 //
 
+#import "TFHpple.h"
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) TFHpple* doc;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)getMenuList
+{
+    NSData* data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://www.xiachufang.com/category/51761/"]];
+    self.doc = [[TFHpple alloc] initWithHTMLData:data];
+    //            <ul class="level2 plain list ">
+
+    NSArray* a = [self.doc searchWithXPathQuery:@"//ul[@class='level2 plain list ']//a"];
+    for (TFHppleElement* element in a) {
+        NSString* url = element.attributes[@"href"];
+        NSString* title = element.content;
+        title = [self replaceSpaceAndEnterString:title];
+    }
+}
+#pragma mark - Tool
+- (NSString*)replaceSpaceAndEnterString:(NSString*)str
+{
+    return [[str stringByReplacingOccurrencesOfString:@" " withString:@""] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+}
+
++ (NSDictionary*)dictionaryWithJsonString:(NSString*)jsonString
+{
+    if (jsonString == nil) {
+        return nil;
+    }
+    NSData* jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError* err;
+    NSDictionary* dic = [NSJSONSerialization JSONObjectWithData:jsonData
+                                                        options:NSJSONReadingMutableContainers
+                                                          error:&err];
+    if (err) {
+        NSLog(@"json解析失败：%@", err);
+        return nil;
+    }
+    return dic;
+}
+
++ (NSString*)dictionaryToJson:(NSDictionary*)dic
+{
+    NSError* parseError = nil;
+    NSData* jsonData = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:&parseError];
+    return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+}
++ (NSArray*)arrayWithJsonString:(NSString*)jsonString
+{
+    if (jsonString == nil) {
+        return nil;
+    }
+    NSData* jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSError* err;
+    NSArray* dic = [NSJSONSerialization JSONObjectWithData:jsonData
+                                                   options:NSJSONReadingMutableContainers
+                                                     error:&err];
+    if (err) {
+        NSLog(@"json解析失败：%@", err);
+        return nil;
+    }
+    return dic;
+}
+
++ (NSString*)arrToJson:(NSArray*)dic
+{
+    NSError* parseError = nil;
+    NSData* jsonData = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:&parseError];
+    return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
 }
 
 @end
